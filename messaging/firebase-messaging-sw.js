d25f4d6b17193425ca3fcf20bc39560657569d90
satvik64 <76674965+satvik64@users.noreply.github.com>
2022-05-02 07:27:57 +0000

    api change

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -6,12 +6,14 @@ importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBxhkOKc_tWaeeUSePIwitc2Tm-aqrLTww",
-    authDomain: "chat-app-7da13.firebaseapp.com",
-    databaseURL: "https://chat-app-7da13.firebaseio.com",
-    projectId: "chat-app-7da13",
-    messagingSenderId: "454878238535",
-    appId: "1:454878238535:web:6fb06475d86f38e4"
+  apiKey: "AIzaSyAnWB8w5djDai7xUq8QmXvm43T8W4trW3w",
+  authDomain: "chat-app64-eae61.firebaseapp.com",
+  databaseURL: "https://chat-app64-eae61-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-app64-eae61",
+  storageBucket: "chat-app64-eae61.appspot.com",
+  messagingSenderId: "426477258139",
+  appId: "1:426477258139:web:62c78f35810adc0b2f8d3e",
+  measurementId: "G-YC9BFXBLDK"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
